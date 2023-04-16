171f0a037a6807f448757067dfbb0f9eb029a73e
zoli <k9612431@gmail.com>
2023-04-16 12:07:50 +0200

    Welcome szöveg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Szia Katus! Ez egy docker - react - git - travis - aws demo.
         </p>
         
         <a
